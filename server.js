3e8cd51ca7d3fda31f0493391c6dcd1130a4942d
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-07-24 19:11:33 -0400

    added * endpoint and added res.status for renponse

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs"); // node file system
 const path = require("path");
+const { title } = require("process");
 // using UUID to keep a track of list items.
 const { v4: uuidv4 } = require("uuid");
 // uuidv4(); returns the unique id
@@ -17,63 +18,72 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //connecting to the public folder
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
-);
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
 // This get route reads file db.json and parses the data that is then used to render the data
 app.get("/api/notes", (req, res) => {
-  console.log(`inside get call`);
   fs.readFile("./db/db.json", "utf-8", (error, data) => {
     if (error) {
-      console.log(`Inside error`, error);
+      res.json(`Found an error while trying to get the data`, error);
     } else {
-      console.log(`Reading data`, data);
       res.json(JSON.parse(data));
     }
   });
 });
+
 // The post request saves data to the db.json file
 app.post("/api/notes", (req, res) => {
-  fs.readFile("./db/db.json", "utf-8", (error, data) => {
-    if (error) {
-      console.log(error);
-    } else {
-      let temp = JSON.parse(data);
-      let noteItem = req.body;
-      noteItem.id = uuidv4();
-      temp.push(noteItem);
-      fs.writeFile("./db/db.json", JSON.stringify(temp), (err) => {
-        //console.error(err);
-        res.send();
-      });
-    }
-  });
+  const { title, text } = req.body;
+  if (title && text) {
+    fs.readFile("./db/db.json", "utf-8", (error, data) => {
+      if (error) {
+        res.status(500).json(`Couldn't get data`);
+      } else {
+        let temp = JSON.parse(data);
+        let noteItem = req.body;
+        noteItem.id = uuidv4();
+        temp.push(noteItem);
+        fs.writeFile("./db/db.json", JSON.stringify(temp), (err) => {
+          res.status(200).json(`Saved notes to the dataBase`);
+        });
+      }
+    });
+  } else {
+    res.status(500).json(`Couldn't have notes at this time!`);
+  }
 });
 
-// This route deletes data
+// This route deletes data from the local db.json
 app.delete("/api/notes/:id", (req, res) => {
-  console.log(`inside delete API`);
   let idToDelete = req.params.id;
-  fs.readFile("./db/db.json", "utf-8", (error, data) => {
-    if (error) {
-      console.log(error);
-    } else {
-      let dbData = JSON.parse(data);
-      let newData = dbData.filter((el) => {
-        if (el.id !== idToDelete) {
-          return el;
-        }
-      });
+  if (idToDelete) {
+    fs.readFile("./db/db.json", "utf-8", (error, data) => {
+      if (error) {
+        res.status(500).json(`Data unavailable at this time`);
+      } else {
+        let dbData = JSON.parse(data);
+        let newData = dbData.filter((el) => {
+          if (el.id !== idToDelete) {
+            return el;
+          }
+        });
 
-      fs.writeFile("./db/db.json", JSON.stringify(newData), (err) => {
-        // console.error(err);
-        res.send();
-      });
-    }
-  });
+        fs.writeFile("./db/db.json", JSON.stringify(newData), (err) => {
+          res.status(200).json(`Note was deleted`);
+        });
+      }
+    });
+  } else {
+    res.json(`Nothing to delete at this time`);
+  }
 });
+
+// Any other route brings the user back to the homepage
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+// listing on port
 app.listen(PORT, () => console.log(`Listening to port: ${PORT}`));
